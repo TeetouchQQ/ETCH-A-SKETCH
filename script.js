88fe1cc95a4e8f24884215c1f9d1f7f87b192763
Teetouch <teawkung@live.com>
2021-05-25 15:54:16 +0700

    create dynamic grid

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-var container = document.createElement("div");
+
+const container = document.querySelector("#container");
+
+function createGrid(x){
+    for(let row = 0;row<x;row++){
+        let div = document.createElement("div");
+        for(let col=0;col<x;col++){
+            let grid = document.createElement("div");
+            grid.className = 'grid-item';
+            grid.innerText = x;
+            div.append(grid);
+        }
+        container.append(div);
+
+    }
+
+}
+createGrid(5);
