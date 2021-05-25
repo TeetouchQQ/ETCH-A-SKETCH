ab11d251bebd231e79a5315c18f99f50a0b62fde
Teetouch <teawkung@live.com>
2021-05-25 17:29:31 +0700

    add grid button

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 
-const container = document.querySelector("#container");
+
 
 function createGrid(x){
+    const container = document.querySelector("#container");
     for(let row = 0;row<x;row++){
         let div = document.createElement("div");
         for(let col=0;col<x;col++){
             let grid = document.createElement("div");
             grid.className = 'grid-item';
             grid.id = 'grid-item';
-            grid.innerText = x;
+
             grid.addEventListener(
                 "mouseover", function () {   
             this.style.background 
@@ -25,5 +26,19 @@ function createGrid(x){
 
 var color = [, "#3C9EE7", "#E7993C", 
 "#E73C99", "#3CE746", "#E7993C"];
-createGrid(8);
+createGrid(2)
+function Grid() {  
+    const myNode = document.getElementById("container");
+    while (myNode.lastElementChild) {
+      myNode.removeChild(myNode.lastElementChild);
+    }
+    
+    var a = prompt("Insert Grid Size"); 
+    createGrid(a);
+
+    
+
+    }  
+
+
 
